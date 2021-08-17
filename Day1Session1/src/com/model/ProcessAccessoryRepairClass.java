241f6336ce6bf7e0dec1cc0557f8dcd429a7359c
Ishita938313 <88820522+Ishita938313@users.noreply.github.com>
2021-08-17 18:53:05 +0530

    Day 1 Session 1

---
+++ Day1Session1/src/com/model/ProcessAccessoryRepairClass.java
@@ -0,0 +1,12 @@
+package com.model;
+
+
+public class ProcessAccessoryRepairClass implements IOrderRepair {
+
+	@Override
+	public void ProcessAccessoryRepair(String accessoryType) {
+		// TODO Auto-generated method stub
+		System.out.println("repair accepted "+ accessoryType);
+	}
+
+}
