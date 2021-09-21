821653e9bd3b1190fa25d602dab35197525b8435
Ishita938313 <88820522+Ishita938313@users.noreply.github.com>
2021-09-21 22:05:06 +0530

    Delete AWS/Spring Rest App with RDS BackEnd directory

--- AWS/Spring Rest App with RDS BackEnd/AwsBeanstalk-RDS/src/main/java/com/cognizant/AwsBeanstalk/RDS/Repositry/EmployeeRepositry.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.AwsBeanstalk.RDS.Repositry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.AwsBeanstalk.RDS.Beans.Employee;
-
-public interface EmployeeRepositry extends JpaRepository<Employee,Integer>{
-	
-}
